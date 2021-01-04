8f878db9e843e26809d218fbc7a59b9aae083d91
emtupp <emilyc.tupper@gmail.com>
2021-01-04 23:23:41 +0000

    add finished exercises 1 through 3

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,7 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store = Store.create(name: "Surrey", annual_revenue: 3000000, mens_apparel:true, womens_apparel:true)
+store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel:false, womens_apparel:true)
+store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel:true, womens_apparel:false)
